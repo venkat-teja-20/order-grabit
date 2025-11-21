188c1765e2654ee002a581f059ade2e5d15cd3a9
Sai Venkata Teja <saivenkatateja714@gmail.com>
2025-11-21 22:03:58 +0530

    Audit Implemented For JPA

--- api/src/main/java/com/grabit/config/JpaConfiguration.java
+++ api/src/main/java/com/grabit/config/JpaConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
@@ -13,6 +16,16 @@ public class JpaConfiguration {
 
     @Bean
     public AuditorAware<String> auditorAware(){
-        return ()-> Optional.of("System");
+        return ()-> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if(authentication==null || !authentication.isAuthenticated())
+                return Optional.of("Anonymous User");
+            Object principal=authentication.getPrincipal();
+            if(principal instanceof UserDetails userDetails)
+                return Optional.ofNullable(userDetails.getUsername());
+            else if(principal instanceof String)
+                return Optional.of(String.valueOf(principal));
+            return Optional.of("Anonymous User");
+        };
     }
 }
