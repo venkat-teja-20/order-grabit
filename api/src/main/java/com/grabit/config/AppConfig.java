2bf4da5eaa44dababc6ad273c1f2cc55c6c2bda7
Sai Venkata Teja <saivenkatateja714@gmail.com>
2025-12-22 20:40:31 +0530

    New Topic Bean created

--- api/src/main/java/com/grabit/config/AppConfig.java
+++ api/src/main/java/com/grabit/config/AppConfig.java
@@ -3,8 +3,10 @@ package com.grabit.config;
 import com.grabit.mapper.DeliveryPartnerURLMapper;
 import com.grabit.mapper.MemberURLMapper;
 import com.grabit.mapper.RestaurantURLMapper;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class AppConfig {
@@ -22,5 +24,12 @@ public class AppConfig {
     @Bean
     public DeliveryPartnerURLMapper getDeliveryPartnerURLMapper(){
         return new DeliveryPartnerURLMapper(System.getenv("delivery_partner_url"));
+    }
+
+    @Bean
+    public NewTopic kafkaTopic(){
+        return TopicBuilder
+                .name("")
+                .build();
     }
 }
